63550aac2b654bd2f1429c5e706f777bf666607a
shijingl <liushijing123@gmail.com>
2020-01-11 07:48:33 -0500

    fix the vulnerbility in the package

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
